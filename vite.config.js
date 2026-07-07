07616808da9c6cc1742f437694da0769280af795
khalil abbes <khalilabbes66@gmail.com>
2026-07-07 10:12:38 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,18 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  root: resolve(__dirname, 'frontend'),
   build: {
+    outDir: resolve(__dirname, 'dist'),
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        activities: resolve(__dirname, 'activities.html'),
-        addMenu: resolve(__dirname, 'addMenu.html'),
-        calendar: resolve(__dirname, 'calendar.html'),
-        pr: resolve(__dirname, 'pr.html'),
+        main: resolve(__dirname, 'frontend/index.html'),
+        activities: resolve(__dirname, 'frontend/pages/activities.html'),
+        addMenu: resolve(__dirname, 'frontend/pages/addMenu.html'),
+        calendar: resolve(__dirname, 'frontend/pages/calendar.html'),
+        pr: resolve(__dirname, 'frontend/pages/pr.html'),
+        login: resolve(__dirname, 'frontend/pages/login.html'),
+        register: resolve(__dirname, 'frontend/pages/register.html'),
       },
     },
   },
