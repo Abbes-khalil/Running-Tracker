1e43a1ec98747e58c1f9629e7f2fdc7b23351682
khalil abbes <khalil.abbes@etudiant-fst.utm.tn>
2026-07-16 21:28:59 +0100

    commit

---
+++ dist/backend/recentact.php
@@ -0,0 +1,15 @@
+﻿<?php
+$conn = mysqli_connect("localhost","root", "", "runningtrackingapp");
+
+$sql="SELECT run_type,run_date,distance_km,duration_seconds,average_heart_rate,notes from runs ORDER by run_date DESC";
+
+$res=mysqli_query($conn,$sql);
+$runs = [];
+
+while ($row =  mysqli_fetch_assoc($res)) {
+    $runs[] = $row;
+};  
+
+echo json_encode($runs  );
+
+?>
